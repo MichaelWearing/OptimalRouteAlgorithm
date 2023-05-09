fa22c38deabe1aa5ba0f530470b7ab68268b59f9
Mikey <mikeywearing@gmail.com>
2023-05-09 17:38:13 +0100

    added some checks for missing files and included zip file

--- OptimalRoute.js
+++ OptimalRoute.js
@@ -19,39 +19,57 @@ const createWriteString = (
   }
 };
 
-fs.createReadStream(inputFile)
-  .pipe(csv())
-  .on("data", (row) => {
-    const { product_code, quantity, pick_location } = row;
-    if (!products[pick_location]) {
-      products[pick_location] = {};
-    }
-    if (!products[pick_location][product_code]) {
-      products[pick_location][product_code] = 0;
-    }
-    products[pick_location][product_code] += Number(quantity);
-  })
-  .on("end", () => {
-    const outputStream = fs.createWriteStream(outputFile);
-    outputStream.write("product_code,quantity,pick_location\n");
+fs.access(inputFile, (err) => {
+  if (err) {
+    console.error(`Unable to access Input File: ${err}`);
+    return;
+  }
 
-    for (let bay = 0; bay < 52; bay++) {
-      for (let shelf = 0; shelf < 10; shelf++) {
-        if (bay >= 26) {
-          const bayAndShelfNode = `A${String.fromCharCode(65 + bay - 26)} ${
-            shelf + 1
-          }`;
-          const productToBePickedUp = products[bayAndShelfNode];
-          createWriteString(bayAndShelfNode, productToBePickedUp, outputStream);
-        } else {
-          const bayAndShelfNode = `${String.fromCharCode(65 + bay)} ${
-            shelf + 1
-          }`;
-          const productToBePickedUp = products[bayAndShelfNode];
-          createWriteString(bayAndShelfNode, productToBePickedUp, outputStream);
+  const outputStream = fs.createWriteStream(outputFile);
+  outputStream.on("error", (err) => {
+    console.error(`Unable to write to output file: ${err}`);
+  });
+  outputStream.write("product_code,quantity,pick_location\n");
+
+  fs.createReadStream(inputFile)
+    .pipe(csv())
+    .on("data", (row) => {
+      const { product_code, quantity, pick_location } = row;
+      if (!products[pick_location]) {
+        products[pick_location] = {};
+      }
+      if (!products[pick_location][product_code]) {
+        products[pick_location][product_code] = 0;
+      }
+      products[pick_location][product_code] += Number(quantity);
+    })
+    .on("end", () => {
+      for (let bay = 0; bay < 52; bay++) {
+        for (let shelf = 0; shelf < 10; shelf++) {
+          if (bay >= 26) {
+            const bayAndShelfNode = `A${String.fromCharCode(65 + bay - 26)} ${
+              shelf + 1
+            }`;
+            const productToBePickedUp = products[bayAndShelfNode];
+            createWriteString(
+              bayAndShelfNode,
+              productToBePickedUp,
+              outputStream
+            );
+          } else {
+            const bayAndShelfNode = `${String.fromCharCode(65 + bay)} ${
+              shelf + 1
+            }`;
+            const productToBePickedUp = products[bayAndShelfNode];
+            createWriteString(
+              bayAndShelfNode,
+              productToBePickedUp,
+              outputStream
+            );
+          }
         }
       }
-    }
 
-    outputStream.end();
-  });
+      outputStream.end();
+    });
+});
